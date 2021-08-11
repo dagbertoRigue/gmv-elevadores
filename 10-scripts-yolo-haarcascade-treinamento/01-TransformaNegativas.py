ad0d6d8cdc1616fdc8f40172a01b3689145b80ed
Dagberto Rigue <riguealive@gmail.com>
2021-08-11 16:49:51 -0300

    Add files via upload

---
+++ 10-scripts-yolo-haarcascade-treinamento/01-TransformaNegativas.py
@@ -0,0 +1,45 @@
+# Importando a biblioteca do cv2
+import cv2
+# Importando o módulo de sistema
+import os
+# Importando o módulo que trata os caminhos das imagens
+from imutils import paths
+# Importando o módulo que move arquivo de uma pasta para outra
+import shutil
+
+# Definindo uma função para o programa
+def listNegImagem():
+    # Criando uma lista dos itens contidas no diretório informado
+    imagePath = list(paths.list_images('images/gabarito'))
+    # Implementando um contador que será usado para percorrer a lista de imagens
+    cont = 1
+
+    # Cria um diretório 'neg' caso ainda não exista
+    if not os.path.exists('neg'):
+        os.makedirs('neg')
+    # Para cada item 'i' da lista faça :
+    for i in imagePath:
+        # Copie a imagem, renomeie com o número atual do contador
+        shutil.copy(i, i.replace(i, "neg/"+str(cont)+".png"))
+        # Altere a imagem para a escala de cinza
+        img = cv2.imread("neg/" + str(cont) + ".png", cv2.IMREAD_GRAYSCALE)
+        # Redimensione a imagem para 200x200
+        resized_image = cv2.resize(img, (200, 200))
+        # Salva a imagem renomeada, em escala de cinza e redimensionada
+        cv2.imwrite("neg/" + str(cont) + ".png", resized_image)
+
+        # Imprime para conferência durante os testes
+        print(i.replace(i, "neg/" + str(cont) + ".png"))
+
+        # Incrementa o contador
+        cont += 1
+
+
+listNegImagem()
+
+
+
+
+
+
+
